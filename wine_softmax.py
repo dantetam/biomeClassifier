e70173c7f4579937246c8ba20efaa499670bebb2
Dante Tam <dante.tam1@gmail.com>
2017-12-17 22:18:00 -0800

    Add more methods for importing and training data

--- wine_softmax.py
+++ wine_softmax.py
@@ -13,9 +13,11 @@
 # limitations under the License.
 # ==============================================================================
 
-"""A very simple MNIST classifier.
-See extensive documentation at
-https://www.tensorflow.org/get_started/mnist/beginners
+"""
+
+TensorFlow application meant to show the convergence of a logistic regression 
+in a properly defined experiment (train, valid, test)
+
 """
 from __future__ import absolute_import
 from __future__ import division
@@ -27,46 +29,79 @@ import sys
 from tensorflow.examples.tutorials.mnist import input_data
 
 import tensorflow as tf
+import numpy as np
+import scipy.io
 
 FLAGS = None
 
+validProp = 0.1
+
+# Shuffle two arrays that are paired together
+def shuffle(a, b):
+  assert len(a) == len(b)
+  p = np.random.permutation(len(a))
+  return a[p], b[p]
 
 def main(_):
   # Import data
-  mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+  wine_data = scipy.io.loadmat("./wine_data.mat")
+  X_data, Y_data = wine_data["X"], wine_data["y"]
+  # print(X_data.shape)
+  
+  X_data_shuf, Y_data_shuf = shuffle(X_data, Y_data)
+  X_train = X_data_shuf[int(validProp * X_data.shape[0]):]
+  Y_train = Y_data_shuf[int(validProp * Y_data.shape[0]):]
+  X_valid = X_data_shuf[0 : int(validProp * X_data.shape[0])]
+  Y_valid = Y_data_shuf[0 : int(validProp * Y_data.shape[0])]
+  X_test = wine_data["X_test"]
+  
+  # print(X_valid, Y_valid)
   
   # Create the model
-  x = tf.placeholder(tf.float32, [None, 784])
-  W = tf.Variable(tf.zeros([784, 10]))
-  b = tf.Variable(tf.zeros([10]))
+  x = tf.placeholder(tf.float32, [None, 12])
+  W = tf.Variable(tf.zeros([12, 1]))
+  b = tf.Variable(tf.zeros([1]))
   y = tf.matmul(x, W) + b
 
   # Define loss and optimizer
+  y_ = tf.placeholder(tf.float32, [None, 1])
+
+  # The raw formulation of cross-entropy,
+  #
+  #   tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(tf.nn.softmax(y)),
+  #                                 reduction_indices=[1]))
+  #
+  # can be numerically unstable.
+  #
   # So here we use tf.nn.softmax_cross_entropy_with_logits on the raw
   # outputs of 'y', and then average across the batch.
-  y_ = tf.placeholder(tf.float32, [None, 10])
   cross_entropy = tf.reduce_mean(
       tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
   train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
   sess = tf.InteractiveSession()
   tf.global_variables_initializer().run()
-  
   # Train
-  for _ in range(1000):
-    batch_xs, batch_ys = mnist.train.next_batch(100)
+  for iterIndex in range(25):
+    X_train_shuf, Y_train_shuf = shuffle(X_train, Y_train)
+    batch_xs = X_train_shuf[0:int(X_train_shuf.shape[0]*0.1)]
+    batch_ys = Y_train_shuf[0:int(Y_train_shuf.shape[0]*0.1)]
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
   # Test trained model
   correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+  
   accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-  print(sess.run(accuracy, feed_dict={x: mnist.test.images,
-                                      y_: mnist.test.labels}))
+  print(sess.run(accuracy, feed_dict={x: X_valid, y_: Y_valid}))
+ 
+  testResults = sess.run(y, feed_dict={x: X_test})
 
+  print("--------")
+  print(testResults)
+  
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data',
-                      help='Directory for storing input data')
+  #parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data',
+                      #help='Directory for storing input data')
   FLAGS, unparsed = parser.parse_known_args()
-  
 tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
